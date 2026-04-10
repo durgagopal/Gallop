1ba0956b0ebe9807560058885eba480c66ee0168
durgagopal <durga.gopala@gmail.com>
2026-04-10 18:52:38 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'gallop-v1';
+const ASSETS = [
+    '/',
+    '/index.html',
+    '/style.css',
+    '/manifest.json'
+];
+
+// Install Event
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS);
+        })
+    );
+});
+
+// Fetch Event (Offline Support)
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((response) => {
+            return response || fetch(e.request);
+        })
+    );
+});
